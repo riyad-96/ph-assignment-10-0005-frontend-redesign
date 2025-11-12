b1984a561b6e4f8cc2b508548d381507c3222eb3
riyad-96 <riyadofficial.ide@gmail.com>
2025-11-12 20:01:53 +0600

    final commit

--- src/components/loaders/AppLoader.jsx
+++ src/components/loaders/AppLoader.jsx
@@ -1,7 +1,7 @@
 function AppLoader() {
   return (
-    <div className="screen-height grid place-items-center bg-(--main-bg)">
-      <span className="dark:text-zinc-200 loading loading-infinity loading-xl opacity-80 scale-120 md:scale-150"></span>
+    <div className="screen-height grid cursor-wait place-items-center bg-(--main-bg)">
+      <span className="loading loading-infinity loading-xl scale-120 opacity-80 md:scale-150 dark:text-zinc-200"></span>
     </div>
   );
 }
