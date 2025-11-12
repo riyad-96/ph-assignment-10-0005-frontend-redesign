67ff04237bce1e5445d63cffc5ec36a476a29d81
riyad-96 <riyadofficial.ide@gmail.com>
2025-11-12 19:50:40 +0600

    theme toggler button design improved

--- src/components/header/ThemeToggler.jsx
+++ src/components/header/ThemeToggler.jsx
@@ -7,24 +7,23 @@ function ThemeToggler() {
   return (
     <button
       onClick={() => setIsDark((prev) => !prev)}
-      className="relative h-5 w-10 rounded-full bg-(--main-bg)/50 inset-shadow-xs outline-2 outline-(--accent-color) transition-colors duration-150"
+      className="relative h-5 w-10 rounded-full bg-black/5 ring-1 ring-(--accent-color) transition-[background-color,box-shadow] duration-150 pointer-fine:hover:ring-cyan-500/30"
     >
       <span
-        className={`absolute top-0 left-0 grid h-full w-1/2 place-items-center overflow-hidden rounded-full bg-(--white) shadow-xs transition-[color,background-color,translate] duration-[150ms,150ms,300ms] ${isDark ? 'translate-x-1/1' : 'translate-x-0'}`}
+        className={`absolute top-0 left-0 grid h-full w-1/2 place-items-center overflow-hidden rounded-full bg-white transition-[background-color,translate] duration-[150ms,300ms] dark:bg-white/10 ${isDark ? 'translate-x-1/1' : 'translate-x-0'}`}
       >
         <AnimatePresence>
           {isDark && (
             <motion.span
               initial={{
-                y: -20,
+                scale: 0,
               }}
               animate={{
-                y: 0,
+                scale: 1,
               }}
               transition={{
-                y: {
-                  duration: 0.3,
-                },
+                duration: 0.2,
+                delay: 0.1,
               }}
               className="absolute grid opacity-70"
             >
@@ -36,15 +35,14 @@ function ThemeToggler() {
           {!isDark && (
             <motion.span
               initial={{
-                y: -20,
+                scale: 0,
               }}
               animate={{
-                y: 0,
+                scale: 1,
               }}
               transition={{
-                y: {
-                  duration: 0.3,
-                },
+                duration: 0.2,
+                delay: 0.1,
               }}
               className="absolute grid opacity-70"
             >
