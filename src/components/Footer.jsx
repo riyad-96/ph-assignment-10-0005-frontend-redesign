37f9b117911e9893121b335867814e53b2d3dc60
riyad-96 <riyadofficial.ide@gmail.com>
2025-11-13 10:03:53 +0600

    footer social link spacing fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { FacebookIcon, InstagramIcon, LinkedInIcon, XIcon } from './Svgs';
 
 function Footer() {
   return (
-    <footer className="transition-colors duration-150 bg-(--header-bg) px-2 py-8 md:px-3">
+    <footer className="bg-(--header-bg) px-2 py-8 transition-colors duration-150 md:px-3">
       <div className="mx-auto max-w-[1440px]">
         <div className="max-md:space-y-6 md:flex md:gap-4">
           <div className="flex-1">
@@ -23,7 +23,7 @@ function Footer() {
 
           <div className="flex-1">
             <div className="w-fit md:flex md:h-full md:w-full md:items-center md:justify-center">
-              <div className="relative flex items-center gap-1 rounded-md py-0.5 md:py-1 pointer-fine:px-2 pointer-fine:hover:bg-zinc-500/10">
+              <div className="relative flex items-center gap-1 rounded-md py-2 md:py-1 pointer-fine:px-2 pointer-fine:hover:bg-zinc-500/10">
                 <span className="grid">
                   <FacebookIcon size="24" />
                 </span>
@@ -35,7 +35,7 @@ function Footer() {
                 ></a>
               </div>
 
-              <div className="relative flex items-center gap-1 rounded-md py-0.5 md:py-1 pointer-fine:px-2 pointer-fine:hover:bg-zinc-500/10">
+              <div className="relative flex items-center gap-1 rounded-md py-2 md:py-1 pointer-fine:px-2 pointer-fine:hover:bg-zinc-500/10">
                 <span className="grid">
                   <XIcon size="24" />
                 </span>
@@ -47,7 +47,7 @@ function Footer() {
                 ></a>
               </div>
 
-              <div className="relative flex items-center gap-1 rounded-md py-0.5 md:py-1 pointer-fine:px-2 pointer-fine:hover:bg-zinc-500/10">
+              <div className="relative flex items-center gap-1 rounded-md py-2 md:py-1 pointer-fine:px-2 pointer-fine:hover:bg-zinc-500/10">
                 <span className="grid">
                   <LinkedInIcon size="24" />
                 </span>
@@ -59,7 +59,7 @@ function Footer() {
                 ></a>
               </div>
 
-              <div className="relative flex items-center gap-1 rounded-md py-0.5 md:py-1 pointer-fine:px-2 pointer-fine:hover:bg-zinc-500/10">
+              <div className="relative flex items-center gap-1 rounded-md py-2 md:py-1 pointer-fine:px-2 pointer-fine:hover:bg-zinc-500/10">
                 <span className="grid">
                   <InstagramIcon size="24" />
                 </span>
@@ -74,7 +74,7 @@ function Footer() {
           </div>
         </div>
 
-        <div className="pt-8 text-center text-sm font-light">
+        <div className="pt-16 text-center text-sm font-light">
           <p>
             © 2025{' '}
             <a
--- src/components/header/ThemeToggler.jsx
+++ src/components/header/ThemeToggler.jsx
@@ -7,10 +7,10 @@ function ThemeToggler() {
   return (
     <button
       onClick={() => setIsDark((prev) => !prev)}
-      className="relative h-5 w-10 rounded-full bg-black/5 ring-1 ring-(--accent-color) transition-[background-color,box-shadow] duration-150 pointer-fine:hover:ring-cyan-500/30"
+      className="relative h-5 w-10 rounded-full bg-black/5 ring-1 ring-(--accent-color) pointer-fine:hover:ring-cyan-500/30"
     >
       <span
-        className={`absolute top-0 left-0 grid h-full w-1/2 place-items-center overflow-hidden rounded-full bg-white transition-[background-color,translate] duration-[150ms,300ms] dark:bg-white/10 ${isDark ? 'translate-x-1/1' : 'translate-x-0'}`}
+        className={`absolute top-0 left-0 grid h-full w-1/2 place-items-center overflow-hidden rounded-full transition-[background-color,translate] duration-[150ms,250ms] ${isDark ? 'translate-x-1/1 bg-zinc-800' : 'translate-x-0 bg-white'}`}
       >
         <AnimatePresence>
           {isDark && (
